6a7cdfae694b046a9b1b9593da278d9f24c1eda1
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-07-28 10:19:54 -0500

    feat: shuffle within a relationship

--- secret_santa_gen.py
+++ secret_santa_gen.py
@@ -9,7 +9,8 @@ def contains_relationship(people_list):
     return False
 
 
-# List of lists containing people related to each other
+# List containing the people to be assigned to each other
+# Can contain lists containing people related to each other
 people = [["Andre", "Shannon", "Arwen"], ["Jesse", "Miranda"], ["Andrea"],
           ["Alex", "Madelyn"], ["Ian", "Lauren"]]
 
@@ -42,29 +43,12 @@ else:
     max_relation = 1
     people.extend(people[:max_relation])
 
-# for item in people:
-#     if isinstance(item, list):
-#         # Get the length of the largest group of related people
-#         # This is the number to skip by when assigning pairs, ensuring related people do not get paired
-#         max_relation = len(max(people, key=len))
-#         # Flatten the list to prepare for assignments
-#         all_friends = [name for sublist in people for name in sublist]
-#         name_cnt = len(all_friends)
-#         # If the max len for any one relationship is greater than half to total number of
-#         # individuals there will not be enough assignments available
-#         if max_relation > (name_cnt / 2):
-#             print("NOT ENOUGH TO ASSIGN - RELATED PEOPLE WILL BE PAIRED")
-#         # Append the first 'max_relation' friends to the end of the friends list to allow
-#         # wrapping around for the last few friends to have a partner
-#         all_friends.extend(all_friends[:max_relation])
-
-
 # Initialize an empty dictionary to hold the secret santa assignments
 secret_santas = {}
 
-# Go through each friend
+# Go through each person in the list
 for index, santa in enumerate(people):
-    # If the friend is not one of the last 'max_relation' (appended) friends in the list
+    # If the friend is not part of the last 'max_relation' (appended) friends in the list
     if index <= (len(people) - (max_relation + 1)):
         # Assign the friend a secret santa who is 'max_relation' places ahead in the list
         secret_santas[santa] = people[index + max_relation]
