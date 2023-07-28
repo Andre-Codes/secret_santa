7a74eddeb9b0b1e9f70801f86c137f01a203945d
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-07-28 09:37:00 -0500

    added pandas feature, adjusted relationship check

--- secret_santa_gen.py
+++ secret_santa_gen.py
@@ -1,24 +1,31 @@
 import random
+import pandas as pd
 
 # List of lists containing people related to each other
-related = [["Andre", "Shannon", "Arwen"], ["Alex", "Madelyn"], ["Ian", "Lauren"], ["Andrea"], ["Bob"], ["Jesse", "Miranda"]]
+people = ["Andre", "Shannon", "Arwen", "Jesse", "Miranda", "Andrea",
+          "Alex", "Madelyn", "Ian", "Lauren"]
+
+# EXAMPLE - not enough non-related people
+# related = [["Andre", "Shannon", "Arwen", "Jesse", "Miranda", "Andrea"],
+#            ["Alex", "Madelyn"], ["Ian", "Lauren"]]
 
 # Shuffle the friends (maintaining relations)
-random.shuffle(related)
+random.shuffle(people)
 
-# Get the length of the largest group of related people
-# This is the number to skip by when assigning pairs, ensuring related people do not get paired
-max_relation = len(max(related, key=len))
-
-# Flatten the list to prepare for assignments
-all_friends = [name for sublist in related for name in sublist]
+for item in people:
+    if isinstance(item, list):
+        # Get the length of the largest group of related people
+        # This is the number to skip by when assigning pairs, ensuring related people do not get paired
+        max_relation = len(max(people, key=len))
+        # Flatten the list to prepare for assignments
+        all_friends = [name for sublist in people for name in sublist]
 
 name_cnt = len(all_friends)
 
 # If the max len for any one relationship is greater than half to total number of
 # individuals there will not be enough assignments available
 if max_relation > (name_cnt / 2):
-    pass
+    print("NOT ENOUGH TO ASSIGN - RELATED PEOPLE WILL BE PAIRED")
 
 # Initialize an empty dictionary to hold the secret santa assignments
 secret_santas = {}
@@ -32,10 +39,12 @@ for index, santa in enumerate(all_friends):
     # If the friend is not one of the last 'max_relation' (appended) friends in the list
     if index <= (len(all_friends) - (max_relation + 1)):
         # Assign the friend a secret santa who is 'max_relation' places ahead in the list
-        secret_santas[santa] = all_friends[index+max_relation]
+        secret_santas[santa] = all_friends[index + max_relation]
 
 # Print the secret santa assignments
-print(secret_santas)
+# Create a list of tuples with name pairs
+assignments = [(santa, receiver) for santa, receiver in secret_santas.items()]
 
-
-
+# Convert the list of tuples to a DataFrame
+df = pd.DataFrame(assignments, columns=['Santa', 'Receiver'])
+print(df.sort_values('Santa'))
