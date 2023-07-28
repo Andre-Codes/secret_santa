00f423b9d4d914cc31ef90c7b4d7fd5f7100dc28
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-07-28 08:56:38 -0500

    removed flatten list loop

--- secret_santa_gen.py
+++ secret_santa_gen.py
@@ -1,40 +1,40 @@
 import random
 
-# List of sublists containing people related to each other
-related = [["Andre", "Shannon"], ["Alex", "Madelyn"], ["Ian", "Lauren", "Peter"], ["Andrea"]]
+# List of lists containing people related to each other
+related = [["Andre", "Shannon", "Arwen"], ["Alex", "Madelyn"], ["Ian", "Lauren"], ["Andrea"], ["Bob"], ["Jesse", "Miranda"]]
+
+# Shuffle the friends (maintaining relations)
+random.shuffle(related)
 
 # Get the length of the largest group of related people
 # This is the number to skip by, ensuring related people do not get paired
-skip = len(max(related, key=len))
+max_relation = len(max(related, key=len))
 
-# Shuffle the the friends (mainting relations)
-random.shuffle(related)
+# Flatten the list to prepare for
+all_friends = [name for sublist in related for name in sublist]
+all_friends
 
-# Initialize an empty list to hold the new friends list
-friends = []
+name_cnt = len(all_friends)
+
+# If the max len for any one relationship is greater than half to total number of
+# individuals there will not be enough assignments available
+if max_relation > (name_cnt / 2):
+    pass
+
 # Initialize an empty dictionary to hold the secret santa assignments
 secret_santas = {}
-
-# Flatten the list, removing relation sublists while maintaing positions
-# Go through each group of related people
-for person in related:
-    # If the group is a list or relations, extend the friends list with the group
-    if isinstance(person, list):
-        friends.extend(person)
-    # If the group is not a list, append the person to the friends list
-    else:
-        friends.append(person)
-        
-# Append the first 'skip' friends to the end of the friends list to allow 
+     
+# Append the first 'max_relation' friends to the end of the friends list to allow
 # wrapping around for the last few friends to have a partner
-friends.extend(friends[:skip])
+all_friends.extend(all_friends[:max_relation])
+all_friends
         
 # Go through each friend
-for index, santa in enumerate(friends):
-    # If the friend is not one of the last 'skip' (appended) friends in the list
-    if index <= (len(friends) - (skip + 1)):
-        # Assign the friend a secret santa who is 'skip' places ahead in the list
-        secret_santas[santa] = friends[index+skip]
+for index, santa in enumerate(all_friends):
+    # If the friend is not one of the last 'max_relation' (appended) friends in the list
+    if index <= (len(all_friends) - (max_relation + 1)):
+        # Assign the friend a secret santa who is 'max_relation' places ahead in the list
+        secret_santas[santa] = all_friends[index+max_relation]
 
 # Print the secret santa assignments
 secret_santas
