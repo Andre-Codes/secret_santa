50f342c329898a9a13b135123e89928abd79d460
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-08-06 16:02:05 -0500

    'update comments'

--- secret_santa_gen.py
+++ secret_santa_gen.py
@@ -9,58 +9,60 @@ def contains_relationship(people_list):
     return False
 
 
-# List containing the people to be assigned to each other
-# Can contain lists containing people related to each other
-people = [
-    ["John", "Jane"], ["Michael"], ["Emily", "Robert"],
-    ["Olivia"], ["William"], ["Sophia", "James", "Ava"],
-    ["David", "Mia"], ["Joseph"], ["Isabella"], ["Daniel"],
-    ["Emma"], ["Andrew"], ["Charlotte", "Matthew"], ["Amelia"]
-]
+if __name__ == '__main__':
 
-# EXAMPLE - not enough non-related people
-# related = [["Sophia", "James", "Ava", "John", "Jane", "Michael"],
-#            ["Emma", "Andrew"], ["Charlotte"], ["Robert"]]
+    # List containing the people to be assigned to each other
+    # Can contain lists containing people related to each other
+    people = [
+        ["John", "Jane"], ["Michael"], ["Emily", "Robert"],
+        ["Olivia"], ["William"], ["Sophia", "James", "Ava"],
+        ["David", "Mia"], ["Joseph"], ["Isabella"], ["Daniel"],
+        ["Emma"], ["Andrew"], ["Charlotte", "Matthew"], ["Amelia"]
+    ]
 
-# Shuffle the friends (maintaining relations)
-shuffle(people)
+    # EXAMPLE - not enough non-related people
+    # related = [["Sophia", "James", "Ava", "John", "Jane", "Michael"],
+    #            ["Emma", "Andrew"], ["Charlotte"], ["Robert"]]
 
-if contains_relationship(people):
-    # Get the length of the largest group of related people
-    # This is the number to skip by when assigning pairs, ensuring related people do not get paired
-    max_relation = len(max(people, key=len))
-    # Shuffle people within a relationship for increased randomness
-    for relations in people:
-        shuffle(relations)
-    # Flatten the list to prepare for assignments
-    people = [name for sublist in people for name in sublist]
-    # Get the total count of individuals
-    name_cnt = len(people)
-    # If the max len for any one relationship is greater than half to total number of
-    # individuals there will not be enough assignments available
-    if max_relation > (name_cnt / 2):
-        print("NOT ENOUGH TO ASSIGN - RELATED PEOPLE WILL BE PAIRED")
-    # Append the first 'max_relation' friends to the end of the friends list to allow
-    # wrapping around for the last few friends to have a partner
-    people.extend(people[:max_relation])
-else:
-    max_relation = 1
-    people.extend(people[:max_relation])
+    # Shuffle the friends (maintaining relations)
+    shuffle(people)
 
-# Initialize an empty dictionary to hold the secret santa assignments
-secret_santas = {}
+    if contains_relationship(people):
+        # Get the length of the largest group of related people
+        # This is the number to skip by when assigning pairs, ensuring related people do not get paired
+        max_relation = len(max(people, key=len))
+        # Shuffle people within a relationship for increased randomness
+        for relations in people:
+            shuffle(relations)
+        # Flatten the list to prepare for assignments
+        people = [name for sublist in people for name in sublist]
+        # Get the total count of individuals
+        name_cnt = len(people)
+        # If the max len for any one relationship is greater than half to total number of
+        # individuals there will not be enough assignments available
+        if max_relation > (name_cnt / 2):
+            print("NOT ENOUGH TO ASSIGN - RELATED PEOPLE WILL BE PAIRED")
+        # Append the first 'max_relation' friends to the end of the friends list to allow
+        # wrapping around for the last few friends to have a partner
+        people.extend(people[:max_relation])
+    else:
+        max_relation = 1
+        people.extend(people[:max_relation])
 
-# Go through each person in the list
-for index, santa in enumerate(people):
-    # If the friend is not part of the last 'max_relation' (appended) friends in the list
-    if index <= (len(people) - (max_relation + 1)):
-        # Assign the friend a secret santa who is 'max_relation' places ahead in the list
-        secret_santas[santa] = people[index + max_relation]
+    # Initialize an empty dictionary to hold the secret santa assignments
+    secret_santas = {}
 
-# Print the secret santa assignments
-# Create a list of tuples with name pairs
-assignments = [(santa, receiver) for santa, receiver in secret_santas.items()]
+    # Go through each person in the list
+    for index, santa in enumerate(people):
+        # If the friend is not part of the last 'max_relation' (appended) friends in the list
+        if index <= (len(people) - (max_relation + 1)):
+            # Assign the friend a secret santa who is 'max_relation' places ahead in the list
+            secret_santas[santa] = people[index + max_relation]
 
-# Convert the list of tuples to a DataFrame
-df = pd.DataFrame(assignments, columns=['Santa', 'Receiver'])
-print(df.sort_values('Santa'))
+    # Print the secret santa assignments
+    # Create a list of tuples with name pairs
+    assignments = [(santa, receiver) for santa, receiver in secret_santas.items()]
+
+    # Convert the list of tuples to a DataFrame
+    df = pd.DataFrame(assignments, columns=['Santa', 'Receiver'])
+    print(df.sort_values('Santa'))
